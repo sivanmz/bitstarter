0875665d0c919f4ff5bc88830fad79e36ab2a831
Sivan Mozes <sivan.mozes@gmail.com>
2013-07-11 06:18:14 +0000

    added encoding

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var fileContent = fs.readFileSync("index.html", function(err, data) {
+var fileContent = fs.readFileSync("index.html", "utf8", function(err, data) {
     if (err) throw err;
     return data;
 });
